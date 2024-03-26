7a91236129ead8dc660a9f3d6e612a765bb5932c
강병민 <kang@gangbyeongmin-ui-noteubug.local>
2024-03-26 20:56:54 +0900

    initial commit

--- packages/chapter2/src/observer/pubsub.js
+++ packages/chapter2/src/observer/pubsub.js
@@ -3,7 +3,7 @@ let currentObserver = null;
 export const 구독 = (fn) => {
   currentObserver = { fn, key: new Set() };
   observers.add(currentObserver);
-  fn();
+  fn(); //durlt
 };
 
 export const 발행기관 = (obj) => {
@@ -32,3 +32,24 @@ export const 발행기관 = (obj) => {
 
   return _publisher;
 };
+
+const 상태 = 발행기관({ a: 10, b: 20 });
+
+const mockFn1 = () => `a = ${상태.a}`;
+const mockFn2 = () => `b = ${상태.b}`;
+const mockFn3 = () => `a + b = ${상태.a + 상태.b}`;
+const mockFn4 = () => `a * b = ${상태.a * 상태.b}`;
+const mockFn5 = () => `a - b = ${상태.a - 상태.b}`;
+
+구독(mockFn1); // 1)
+구독(mockFn2);
+구독(mockFn3);
+구독(mockFn4);
+구독(mockFn5);
+
+// 구독을 하면 일단 알림을 받는다.
+
+// 발행기관의 상태가 변경되면 알림을 전송한다.
+상태.a = 100;
+
+상태.b = 200;
