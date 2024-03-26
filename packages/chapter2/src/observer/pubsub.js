3ce6ace60f14335acf23dd016c70f3b9f930c10a
강병민 <kang@gangbyeongmin-ui-noteubug.local>
2024-03-26 20:10:19 +0900

    과제 E 제출

--- packages/chapter2/src/observer/pubsub.js
+++ packages/chapter2/src/observer/pubsub.js
@@ -1,7 +1,34 @@
-export const 구독 = fn => {
+let observers = new Set();
+let currentObserver = null;
+export const 구독 = (fn) => {
+  currentObserver = { fn, key: new Set() };
+  observers.add(currentObserver);
   fn();
-}
+};
 
-export const 발행기관 = obj => {
-  return obj;
-}
+export const 발행기관 = (obj) => {
+  let _publisher = {};
+  const keys = Object.keys(obj);
+  function notifySubscribers(key) {
+    observers.forEach((val) => {
+      if (val.key.has(key)) val.fn();
+    });
+  }
+  keys.forEach((key) => {
+    Object.defineProperty(_publisher, key, {
+      get() {
+        if (currentObserver) {
+          currentObserver.key.add(key);
+        }
+        return obj[key];
+      },
+      set(newValue) {
+        obj[key] = newValue;
+
+        notifySubscribers(key);
+      },
+    });
+  });
+
+  return _publisher;
+};
