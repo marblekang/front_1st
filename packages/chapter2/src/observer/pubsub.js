b2cdb875907e4b4955ef003e4271a717440c0f01
강병민 <kang@gangbyeongmin-ui-noteubug.local>
2024-03-27 22:36:30 +0900

    feat: 과제F 진행중

--- packages/chapter2/src/observer/pubsub.js
+++ packages/chapter2/src/observer/pubsub.js
@@ -1,30 +1,39 @@
 let observers = new Set();
 let currentObserver = null;
 export const 구독 = (fn) => {
-  currentObserver = { fn, key: new Set() };
+  currentObserver = { fn, keys: new Set() };
   observers.add(currentObserver);
   fn(); // 1) fn 실행 => 2) get 함수 실행 => get 할때마다 (참조하고 있는 property 갯수만큼) 실행되면서 key 추가
 };
-
+/* 
+발행기관
+1) state를 저장함.
+2) 
+*/
 export const 발행기관 = (obj) => {
   let _publisher = {};
+  let state = { ...obj };
   const keys = Object.keys(obj);
   function notifySubscribers(key) {
     observers.forEach((val) => {
-      if (val.key.has(key)) val.fn();
+      if (val.keys.has(key)) {
+        val.fn();
+      }
     });
   }
   keys.forEach((key) => {
     Object.defineProperty(_publisher, key, {
       get() {
         if (currentObserver) {
-          currentObserver.key.add(key);
+          currentObserver.keys.add(key);
         }
-        return obj[key];
+        return state[key];
       },
       set(newValue) {
-        obj[key] = newValue;
-
+        if (newValue === state[key]) {
+          return;
+        }
+        state[key] = newValue;
         notifySubscribers(key);
       },
     });
@@ -32,21 +41,3 @@ export const 발행기관 = (obj) => {
 
   return _publisher;
 };
-
-const 상태 = 발행기관({ a: 10, b: 20 });
-
-const mockFn1 = () => `a = ${상태.a}`;
-const mockFn2 = () => `b = ${상태.b}`;
-const mockFn3 = () => `a + b = ${상태.a + 상태.b}`;
-const mockFn4 = () => `a * b = ${상태.a * 상태.b}`;
-const mockFn5 = () => `a - b = ${상태.a - 상태.b}`;
-
-구독(mockFn1);
-구독(mockFn2);
-구독(mockFn3);
-구독(mockFn4);
-구독(mockFn5);
-
-상태.a = 100;
-
-상태.b = 200;
--- packages/chapter2/src/observer/store.js
+++ packages/chapter2/src/observer/store.js
@@ -1,6 +1,102 @@
+import { 발행기관 as createPublisher, 구독 } from "./pubsub.js";
 export class Store {
-  constructor({ state, mutations, actions }) {}
+  #state;
+  #mutations;
+  state = {};
 
-  commit(action, payload) {}
+  constructor({ state, mutations, actions }) {
+    this.state = createPublisher(state);
+    this.#mutations = mutations;
 
+    // // state를 직접적으로 수정하지 못하도록 다음과 같이 정의한다.
+    // Object.keys(#state).forEach((key) => {
+    //   Object.defineProperty(this.state, key, { get: () => this.#state[key] });
+    // });
+  }
+
+  commit(action, payload) {
+    this.#mutations[action](this.#state, payload);
+  }
 }
+
+let store = null;
+store = new Store({
+  state: { a: 10, b: 20 },
+  mutations: {
+    SET_A(state, payload) {
+      state.a = payload;
+    },
+    SET_B(state, payload) {
+      state.b = payload;
+    },
+  },
+});
+
+let inputACount = 0;
+let inputBCount = 0;
+let calculatorCount = 0;
+let appCount = 0;
+const InputA = () => {
+  inputACount++;
+  return `<input id="stateA" value="${store.state.a}" size="5" />`;
+};
+const InputB = () => {
+  inputBCount++;
+  return `<input id="stateB" value="${store.state.b}" size="5" />`;
+};
+const Calculator = () => {
+  calculatorCount++;
+  return `<p>a + b = ${store.state.a + store.state.b}</p>`;
+};
+const App = () => {
+  appCount++;
+  return `${InputA()}<br />${InputB()}<br />${Calculator()}`;
+};
+
+구독(InputA);
+구독(InputB);
+구독(Calculator);
+구독(App);
+
+console.log("초기호출 결과");
+console.log(inputACount, "a1");
+console.log(inputBCount, "b1");
+console.log(calculatorCount, "calc1");
+console.log(appCount, "app1");
+console.log("------------------------------------");
+
+console.log("store.state.a의 값만 변경되는 경우");
+console.log("값 변경 전");
+console.log(inputACount, "a");
+console.log(inputBCount, "b");
+console.log(calculatorCount, "calc");
+console.log(appCount, "app");
+
+store.commit("SET_A", 100);
+console.log("값 변경 후");
+console.log(inputACount, "a");
+console.log(inputBCount, "b");
+console.log(calculatorCount, "calc");
+console.log(appCount, "app");
+
+console.log("------------------------------------");
+
+console.log("store.state.a와 b의 값이 각각 변경는 경우");
+console.log("값 변경 전");
+console.log(inputACount, "a");
+console.log(inputBCount, "b");
+console.log(calculatorCount, "calc");
+console.log(appCount, "app");
+console.log("값 A 변경");
+store.commit("SET_A", 100);
+console.log(inputACount, "a");
+console.log(inputBCount, "b");
+console.log(calculatorCount, "calc");
+console.log(appCount, "app");
+console.log("------------------------------------");
+console.log("값 B 변경");
+store.commit("SET_B", 200);
+console.log(inputACount, "a");
+console.log(inputBCount, "b");
+console.log(calculatorCount, "calc");
+console.log(appCount, "app");
