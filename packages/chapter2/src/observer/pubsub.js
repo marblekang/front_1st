12c16a2fc3ba209079de41cafbefb2fbbd99a198
강병민 <kang@gangbyeongmin-ui-noteubug.local>
2024-04-03 21:45:22 +0900

    fix: pub sub 수정

--- packages/chapter2/src/observer/pubsub.js
+++ packages/chapter2/src/observer/pubsub.js
@@ -1,31 +1,26 @@
-let observers = new Set();
 let currentObserver = null;
 export const 구독 = (fn) => {
-  currentObserver = { fn, key: new Set() };
-  observers.add(currentObserver);
+  currentObserver = fn;
   fn();
+  currentObserver = null;
 };
 
 export const 발행기관 = (obj) => {
   let _publisher = {};
   const keys = Object.keys(obj);
-  function notifySubscribers(key) {
-    observers.forEach((val) => {
-      if (val.key.has(key)) val.fn();
-    });
-  }
+
   keys.forEach((key) => {
+    let observers = new Set();
     Object.defineProperty(_publisher, key, {
       get() {
         if (currentObserver) {
-          currentObserver.key.add(key);
+          observers.add(currentObserver);
         }
         return obj[key];
       },
       set(newValue) {
         obj[key] = newValue;
-
-        notifySubscribers(key);
+        observers.forEach((fn) => fn());
       },
     });
   });
